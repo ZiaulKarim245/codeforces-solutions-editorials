8f35fa4eb74decb48628fbb7c79b6770cc204fb8
Ziaul Karim <ziaulkarim278@gmail.com>
2025-06-11 20:08:30 +0600

    'Solution'

--- 1791 C. Prepend and Append/Solution.py
+++ 1791 C. Prepend and Append/Solution.py
@@ -1,1 +1,11 @@
 t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    start = 0
+    end = n - 1
+    while start < end and s[start] != s[end]:
+        n -= 2
+        start += 1
+        end -= 1
+    print(n)
